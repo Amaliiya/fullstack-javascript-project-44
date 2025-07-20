7f48228b92e7756bff288d9df2e2cebe3bcde4d2
Amalia <ahm.amalia007@gmail.com>
2025-07-21 01:19:28 +0300

    add src/

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import readlineSync from 'readline-sync';
+export function gamelogic(rules, game) {
+    console.log("Welcome to the Brain Games!");
+    const name = readlineSync.question("May I have your name? ");
+    console.log(`Hello, ${name}!`);
+    console.log(rules);
+    let correctAnswers = 0;
+    for (let i = 0; i < 3; i++) {
+        const [result, answer, correct] = game();
+        if (correct === true) {
+            correctAnswers++;
+            console.log('Correct!');
+        }
+        if (correct === false) {
+            console.log(`${answer} is wrong answer;(. Correct answer was ${result}.`);
+            console.log(`Let's try again, ${name}!`)
+            break
+        }
+    }
+    if (correctAnswers === 3) {
+            console.log(`Congratulations ${name}!`);
+        } 
+}
