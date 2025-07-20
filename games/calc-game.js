1122d2c814eddbd864ab7e0e367d9e6d93372308
Amalia <ahm.amalia007@gmail.com>
2025-07-21 01:56:26 +0300

    added 'games' folder

---
+++ games/calc-game.js
@@ -0,0 +1,32 @@
+import { gamelogic } from "../src/index.js";
+import readlineSync from 'readline-sync';
+const rules = 'What is the result of the expression?'
+function calc() {
+    
+    const operators = ['+', '-', '*']
+    const randomOperator = operators[Math.floor(Math.random() * operators.length)];
+    const number1 = Math.floor(Math.random() * (9));
+    const number2 = Math.floor(Math.random() * (10));
+    console.log(`Question: ${number1} ${randomOperator} ${number2}`);
+    const stranswer = readlineSync.question('Your answer: ');
+    const answer = parseInt(stranswer, 10);
+    let correct
+    let result;
+    switch (randomOperator) {
+        case '+':
+            result = number1 + number2;
+            break;
+        case '-':
+            result = number1 - number2;
+            break;
+        case '*':
+            result = number1 * number2;
+            break; 
+    }
+    return [result, answer, result === answer]
+
+    
+}
+export const calcGame = gamelogic(rules, calc);
+
+
---
+++ games/even-game.js
@@ -0,0 +1,36 @@
+import readlineSync from 'readline-sync';
+console.log("Welcome to the Brain Games!");
+const name = readlineSync.question("May I have your name? ");
+console.log(`Hello, ${name}!`);
+console.log('Answer "yes" if the number is even, otherwise answer "no"');
+
+export function evengame() {
+    let correctAnswers = 0;
+    for (let i = 0; i < 3; i++) {
+
+        const number = Math.floor(Math.random() * (52));
+        console.log(`Question: ${number}`);
+        const answer = readlineSync.question('Your answer: ');
+        if (number % 2 === 0) {
+            if (answer === "yes") {
+                correctAnswers++;
+                console.log('Correct!');                
+            } else {
+                console.log('Wrong answer. Try again.')
+                break
+            }            
+        }
+        if (number % 2 !== 0) {
+            if (answer === "no") {
+                correctAnswers++;
+                console.log('Correct!');
+            } else {
+                console.log('Wrong answer. Try again.')
+                break
+            }            
+        }
+        if (correctAnswers === 3) {
+            console.log(`Congratulations ${name}!`);
+        }       
+    } 
+}
